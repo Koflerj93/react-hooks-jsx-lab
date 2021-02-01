f5f9aa7b3098b5ab1d4ed679c81ccb9a45f6873d
github username <github email address>
2021-02-01 08:50:18 -0500

    done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id= "about">About
+    <h2>About Me</h2>
+    <p>Stuff that is cool!!</p>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
